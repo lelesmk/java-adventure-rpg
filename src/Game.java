dfdc4f4e82f0b2c6ee661f408447df64aa96c453
Lele Mkefa <l.mkefa@gmail.com>
2019-11-10 17:46:52 +0200

    {\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600 {\fonttbl\f0\fswiss\fcharset0 Helvetica;} {\colortbl;\red255\green255\blue255;} {\*\expandedcolortbl;;} \paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0 \pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
    \f0\fs24 \cf0 Complete the full story\
    \
    - Create attack guard \
    - Create leave to crossroad\
    - Create location options at crossroad (north, east, west, south)\
    - Create rewards and penalties at locations (set health, set weapon, encounter monster)\
    - Create action at monster location (fight, run)\
    - Create fight scene (attack, run)\
    - Create player attack and monster attack (player and monster damage)\
    - Create end of game (win, lose)\
    }

--- src/Game.java
+++ src/Game.java
@@ -24,11 +24,12 @@ public class Game {
 	JButton startButton, choice1btn, choice2btn, choice3btn, choice4btn;
 	JTextArea bodyTextArea;
 	
-	int playerHP;
+	int playerHP, monsterHP, silverRing;
 	String weapon, position;
 	
 	TitleScreenHandler tsHandler = new TitleScreenHandler(); // listen for start button click
 	ChoiceHandler choiceHandler = new ChoiceHandler(); // listen for choice button click
+	
 	
 	public static void main(String[] args) {
 		
@@ -174,13 +175,14 @@ public class Game {
 		con.add(playerPanel);
 		
 		/* Set player */
-		playerSetup();
+		playSetup();
 		
 	}
 	
-	/* Initialize Player */
-	public void playerSetup() {
+	/* Initialize play */
+	public void playSetup() {
 		
+		monsterHP = 20;
 		playerHP = 15;
 		weapon = "Knife";
 		
@@ -203,7 +205,11 @@ public class Game {
 		choice1btn.setText("Talk to the guard");
 		choice2btn.setText("Attack the guard");
 		choice3btn.setText("Leave");
-		choiceButtonPanel.remove(choice4btn);
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(true);
+		choice3btn.setVisible(true);
+		choice4btn.setVisible(false);
 		
 	}
 	
@@ -215,17 +221,235 @@ public class Game {
 		
 		/* Player answer */
 		choice1btn.setText(">> continue >>");
-		choiceButtonPanel.remove(choice2btn);
-		choiceButtonPanel.remove(choice3btn);
-		choiceButtonPanel.remove(choice4btn);
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
 		
 		
 	}
 	
+	public void attackGuard() {
+		
+		position = "attackGuard";
+		
+		bodyTextArea.setText("Guard: Hey don't be stupid!\n\nThe guard fought back and hit you hard.\n(You receive 3 damage)");
+		/* Reduce player health points */
+		playerHP = playerHP - 3;
+		hpLabelNumber.setText(Integer.toString(playerHP));
+		/* Player answer */
+		choice1btn.setText(">> continue >>");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+		
+	}
 	
+	public void crossRoad() {
+		
+		position = "crossRoad";
+		
+		bodyTextArea.setText("You are at a crossroad.\nIf you go south, you will go back to the town.");
+		
+		/* Player answer */
+		choice1btn.setText("Go north");
+		choice2btn.setText("Go east");
+		choice3btn.setText("Go south");
+		choice4btn.setText("Go west");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(true);
+		choice3btn.setVisible(true);
+		choice4btn.setVisible(true);
+		
+	}
 	
+	public void north() {
+		position = "north";
+		
+		bodyTextArea.setText("There is a river.\nYou drink the water and rest at the riverside.\n\n(Your health has recovered by 2 points.)");
+		/* Recover player health points */
+		playerHP = playerHP + 2; // set health points
+		hpLabelNumber.setText(Integer.toString(playerHP)); // display health points
+		
+		/* Player answer */
+		choice1btn.setText("Go south");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+	}
 	
+	public void east() {
+		position = "east";
+
+		bodyTextArea.setText("You walked into a forest and found a Long Sword!\n\n(You obtained a Long Sword)");
+		/* Recover player health points */
+		weapon = "Long Sword"; // set weapon
+		weaponLabelName.setText(weapon); // display weapon
+
+		/* Player answer */
+		choice1btn.setText("Go west");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+	}
 	
+	public void west() {
+		position = "west";
+
+		bodyTextArea.setText("You encounter a goblin!");
+
+		/* Player answer */
+		choice1btn.setText("Fight");
+		choice2btn.setText("Run");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(true);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+	}
+	
+	public void fight() {
+		position = "fight";
+
+		bodyTextArea.setText("Monster HP: " + monsterHP + "\n\nWhat do you do?");
+		/* Recover player health points */
+		weapon = "Long Sword"; // set weapon
+		weaponLabelName.setText(weapon); // display weapon
+
+		/* Player answer */
+		choice1btn.setText("Attack");
+		choice2btn.setText("Run");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(true);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+	}
+	
+	public void playerAttack() {
+		position = "playerAttack";
+		
+		int playerDamage = 0; // initialize player damage
+		
+		/* Set damage based on weapon */
+		if(weapon.equals("Knife")) {
+			playerDamage = new java.util.Random().nextInt(3); // set damage at random number from 0 to 2
+		} else if(weapon.equals("Long Sword")) {
+			playerDamage = new java.util.Random().nextInt(12); // set damage at random number from 0 to 7
+		}
+		
+		bodyTextArea.setText("You attacked the monster and gave " + playerDamage + " damage!");
+		/* Apply player damage to monster */
+		monsterHP = monsterHP - playerDamage; // set monster health points
+
+		/* Player answer */
+		choice1btn.setText(">> continue >>");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+	}
+	
+	public void monsterAttack() {
+		position = "monsterAttack";
+		
+		int monsterDamage = 0; // initialize player damage
+		
+		/* Set damage based on weapon */
+		monsterDamage = new java.util.Random().nextInt(6); // set damage at random number from 0 to 5
+		
+		
+		bodyTextArea.setText("The monster attacked you and gave " + monsterDamage + " damage!");
+		/* Apply player damage to monster */
+		playerHP = playerHP - monsterDamage; // set monster health points
+		hpLabelNumber.setText(Integer.toString(playerHP)); // display health points
+
+		/* Player answer */
+		choice1btn.setText(">> continue >>");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+	}
+	
+	public void win() {
+		
+		position = "win";
+		
+		bodyTextArea.setText("You defeated the monster!\nThe monster dropped the ring!\n\n(You obtained a Silver Ring)");
+		
+		silverRing = 1;
+		
+		/* Player answer */
+		choice1btn.setText("Go east"); // go back to crossRoad
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(true);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+
+	}
+
+	public void lose() {
+		
+		position = "lose";
+		
+		bodyTextArea.setText("You are dead!\n\n<GAME OVER>");
+		
+		/* Player answer */
+		choice1btn.setText("");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(false);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+
+	}
+	
+	public void ending() {
+
+		position = "ending";
+
+		bodyTextArea.setText("Guard: Oh you killed that goblin?\nThank you so much. You are a true hero!\nWelcome to our town!\n\n<THE END>");
+
+		/* Player answer */
+		choice1btn.setText("");
+		choice2btn.setText("");
+		choice3btn.setText("");
+		choice4btn.setText("");
+		choice1btn.setVisible(false);
+		choice2btn.setVisible(false);
+		choice3btn.setVisible(false);
+		choice4btn.setVisible(false);
+
+	}
 	
 	/* Start Button Click Event Handler */
 	public class TitleScreenHandler implements ActionListener {
@@ -253,19 +477,97 @@ public class Game {
 			// If player is at the town gate	
 			case "townGate":
 				switch(yourChoice) {
-				// If player chooses button 1
-				case "c1": talkGuard(); break; // talk to guard
-				case "c2": break;
-				case "c3": break;
+				case "c1": 
+					// If you have the silver ring
+					if(silverRing == 1) {
+						ending(); // the end of the game
+					} else {
+						talkGuard();
+					} 
+					break; // button 1 switch position to talkGuard
+				case "c2": attackGuard(); break; // button 2 switch position to attackGuard
+				case "c3": crossRoad(); break; // button 3 switch position to crossRoad
 				}
 				break;
 			// If player talks to the guard
 			case "talkGuard":
 				switch(yourChoice) {
-				// If player chooses button 1
-				case "c1": townGate(); break; // go back to townGate
+				case "c1": townGate(); break; // button 1 go back to townGate
 				}
-				
+				break;
+			// If player attacks guard
+			case "attackGuard":
+				switch(yourChoice) {
+				case "c1": townGate(); break; // button 1 switch position to townGate
+				}
+				break;
+			// If player is at crossRoad
+			case "crossRoad":
+				switch(yourChoice) {
+				case "c1": north(); break; // button 1 switch position to north
+				case "c2": east(); break; // button 2 switch position to east
+				case "c3": townGate(); break; // button 3 switch position to townGate
+				case "c4": west(); break; // button 4 switch position to west
+				}
+				break;
+			// If player goes north
+			case "north":
+				switch(yourChoice) {
+				case "c1": crossRoad(); break; // button 1 switch position to crossRoad
+				}
+				break;
+			// If player goes east
+			case "east":
+				switch(yourChoice) {
+				case "c1": crossRoad(); break; // button 1 switch position to crossRoad
+				}
+				break;
+			// If player goes west
+			case "west":
+				switch(yourChoice) {
+				case "c1": fight(); break; // button 1 switch position to fight
+				case "c2": crossRoad(); break; // button 2 switch position to crossRoad
+				}
+				break;
+			// If player chooses to fight
+			case "fight":
+				switch(yourChoice) {
+				case "c1": playerAttack(); break; // button 1 switch position to playerAttack
+				case "c2": crossRoad(); break; // button 2 switch position to crossRoad
+				}
+				break;
+			// If player attacks
+			case "playerAttack":
+				switch(yourChoice) {
+				case "c1": 
+					// Check if monster is alive
+					if(monsterHP < 1) {
+						win();
+					} else {
+						monsterAttack(); // button 1 switch position to monsterAttack
+					}
+					break;
+				}
+				break;
+			// If monster attacks
+			case "monsterAttack":
+				switch(yourChoice) {
+				case "c1": 
+					// Check if player is alive
+					if(playerHP < 1) {
+						lose();
+					} else {
+						fight(); // button 1 switch position to fight
+					}
+					break;
+				}
+				break;
+			// If player kills the monster
+			case "win":
+				switch(yourChoice) {
+				case "c1": townGate(); break; // button 1 switch position to townGate
+				}
+				break;
 			}
 			
 			
